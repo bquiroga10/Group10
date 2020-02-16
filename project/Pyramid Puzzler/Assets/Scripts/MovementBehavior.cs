076dd512e15d578ab414941fb091c1177fb644d5
bquiroga10 <bquiroga10@gmail.com>
2020-02-16 17:43:35 -0500

    Added test functions in for the player movement.
    - Moved scripts into a Scripts folder.
    - Updated the player movement monobehavior script so that the functions that updated movement and positions are in separate scripts that can be easily tested.
    - Created a Tests folder and an Editor folder inside of that.
    - Added a test script for player movement inside the Editor folder, testing updateMovement() and updatePosition().

---
+++ project/Pyramid Puzzler/Assets/Scripts/MovementBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementBehavior : MonoBehaviour
+{
+	public float moveSpeed = 5f;
+	public Rigidbody2D rb;
+	Vector2 movement;
+
+	PlayerMovement playerMovement;
+
+	private void Awake() {
+		playerMovement = new PlayerMovement();
+	}
+	// Update is called once per frame
+	void Update()
+	{
+		//Input
+		playerMovement.updateMovement(ref movement, Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+	void FixedUpdate()
+	{
+		//Moevement 
+		rb.MovePosition(playerMovement.updatePosition(rb.position, movement, moveSpeed));
+	}
+}
---
+++ project/Pyramid Puzzler/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement
+{
+	public void updateMovement(ref Vector2 movement, float xAxis, float yAxis) {
+		movement.x = xAxis;
+		movement.y = yAxis;
+	}
+
+	public Vector2 updatePosition(Vector2 position, Vector2 movement, float moveSpeed) {
+		return position + movement * moveSpeed * Time.fixedDeltaTime;
+	}
+}

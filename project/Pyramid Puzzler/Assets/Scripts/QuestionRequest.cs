97215db8183c1d9c15171caea2747023b0815a92
Ryan Pfaff <jdpfaff@hotmail.com>
2020-02-21 17:16:04 -0500

    added basic question format

---
+++ project/Pyramid Puzzler/Assets/Scripts/QuestionRequest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class QuestionRequest : MonoBehaviour
+{
+    [SerializeField]
+    private string url = "https://opentdb.com/api.php?amount=1";
+
+    private IEnumerator Request()
+    {
+        UnityWebRequest req = UnityWebRequest.Get(url);
+        yield return req.SendWebRequest();
+
+
+    }
+
+    public static string ToText(byte[] bytes)
+    {
+        return System.Text.Encoding.Default.GetString(bytes);
+    }
+}
---
+++ project/Pyramid Puzzler/Assets/Scripts/QuizOpen.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuizOpen : MonoBehaviour
+{
+    public static bool quizOpen = false;
+
+    public GameObject quizUI;
+    // Update is called once per frame
+    
+    void Update()
+    {
+        // Replace  with stepping on tiles later on
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            // Just for testing
+            if (quizOpen)
+                Resume();
+            // This is the important one
+            else
+                Quiz();
+        }
+    }
+
+    // Again, just for testing
+    // This will actually be done in a quiz class later on
+    void Resume()
+    {
+        quizUI.SetActive(false);
+        Time.timeScale = 1f;
+        quizOpen = false;
+    }
+
+    void Quiz()
+    {
+        quizUI.SetActive(true);
+        Time.timeScale = 0f;
+        quizOpen = true;
+    }
+}

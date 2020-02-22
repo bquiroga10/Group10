0350af912196ffb2fd49fd229bc26eb18d55e008
Ryan Pfaff <jdpfaff@hotmail.com>
2020-02-22 05:00:46 -0500

    Added inputs in order to answer questions

---
+++ code/AnswerButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnswerButton : MonoBehaviour
+{
+    public bool isCorrect = false;
+
+    public void Select()
+    {
+        QuizOpen.GetInstance().Answer(this);
+    }
+}
